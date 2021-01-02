d0f6a1507127009a11d5573991b68a3fc39c47d4
ChrisGriffin0815 <66988781+ChrisGriffin0815@users.noreply.github.com>
2021-01-01 22:08:37 -0700

    setup tables

---
+++ amazen-server/setup.sql
@@ -0,0 +1,40 @@
+-- CREATE TABLE keeps (
+--   id int AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+--   creatorId VARCHAR(255) NOT NULL,
+--   name VARCHAR(255) NOT NULL,
+--   description VARCHAR(255) NOT NULL,
+--   image VARCHAR(255) NOT NULL,
+--   views int NOT NULL,
+--   shares intR(255) NOT NULL,
+--   keeps int NOT NULL,
+--   FOREIGN KEY (creatorId)
+--     REFERENCES profiles(id)
+--     ON DELETE CASCADE
+-- )
+
+-- CREATE TABLE vaults (
+--   id int AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+--   creatorId VARCHAR(255) NOT NULL,
+--   name VARCHAR(255) NOT NULL,
+--   description VARCHAR(255) NOT NULL,
+--   isPublished TINYINT NOT NULL
+-- )
+
+-- CREATE TABLE vaultkeeps (
+--   id int AUTO_INCREMENT NOT NULL  PRIMARY KEY,
+--   creatorId VARCHAR(255) NOT NULL,
+--   vaultId int NOT NULL,
+--   keepId int NOT NULL,
+
+--   FOREIGN KEY (vaultId)
+--   REFERENCES vaults (id)
+--   ON DELETE CASCADE,
+
+--   FOREIGN KEY (keepId)
+--   REFERENCES keeps (id)
+--   ON DELETE CASCADE,
+
+--   FOREIGN KEY (creatorId)
+--   REFERENCES profiles(id)
+--   ON DELETE CASCADE
+-- )
